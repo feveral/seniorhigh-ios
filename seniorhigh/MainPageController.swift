ef518f7f9708c9c137d443a030545dfa3f455ab3
楊宗翰 <feveraly@gmail.com>
2025-05-31 02:12:18 +0800

    feat: update color for dark mode issue

--- seniorhigh/MainPageController.swift
+++ seniorhigh/MainPageController.swift
@@ -14,5 +14,22 @@ class MainPageController: UITabBarController {
     
     override func viewDidLoad() {
         MobileAds.shared.requestConfiguration.testDeviceIdentifiers = Config.Application.adTestDevices;
+        view.backgroundColor = .black
+        setStatusBarBackgroundColor(.black)
+    }
+
+    func setStatusBarBackgroundColor(_ color: UIColor) {
+        if let statusBarFrame = view.window?.windowScene?.statusBarManager?.statusBarFrame {
+            let statusBarView = UIView(frame: statusBarFrame)
+            statusBarView.backgroundColor = color
+            view.addSubview(statusBarView)
+        } else {
+            // fallback for earlier stage when window is not available
+            if let statusBarFrame = UIApplication.shared.statusBarFrame as CGRect? {
+                let statusBarView = UIView(frame: statusBarFrame)
+                statusBarView.backgroundColor = color
+                view.addSubview(statusBarView)
+            }
+        }
     }
 }
